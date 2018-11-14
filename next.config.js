3054f007736e61c46e1cd7597711acb238a984c1
Miroslav Kubus <MiroslavK@kentico.com>
2018-11-14 14:00:02 +0100

    Change configuration to use nodemon and run correctly

--- next.config.js
+++ next.config.js
@@ -1,8 +1,3 @@
 const withTypescript = require('@zeit/next-typescript');
 
-module.exports = withTypescript({
-  // we can change the webpack here
-  webpack(config, options) {
-    return config;
-  }
-});
+module.exports = withTypescript();
